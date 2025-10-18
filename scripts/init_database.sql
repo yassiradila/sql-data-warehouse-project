d74b4b4ad4e347a82c81003a9e65c75535865ce0
yassiradila <yassiradila.contact@gmail.com>
2025-10-18 20:02:31 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,15 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS datawarehouse;
+
+-- Create the new database
+CREATE DATABASE datawarehouse;
+
+-- --- IMPORTANT ---
+-- You must now manually connect to the 'datawarehouse' database
+-- in your SQL client before running the next commands.
+
+-- Create schemas (run these after connecting to datawarehouse)
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
 
